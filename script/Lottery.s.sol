6b668204a113630a4e396b8e96a934529c8e5de2
Emmanuel Amodu <emmanuel.amodu@outlook.com>
2024-11-17 07:47:15 +0100

    fix remapping issues

--- script/Lottery.s.sol
+++ script/Lottery.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 import "forge-std/Script.sol";
 import "../contracts/Lottery.sol";
 
-// forge script script/LotteryDeployer.s.sol:LotteryDeployer --broadcast --account 2dmoon-deployer --rpc-url https://rpc.sepolia-api.lisk.com
+// forge script script/Lottery.s.sol:LotteryDeployer --broadcast --account pepper-deployer --rpc-url https://silent-thrilling-sea.base-sepolia.quiknode.pro/99f3a182ebb5acb7e0273692e76914b835b04e82/
 // forge verify-contract 0xf81093539691337D3b36d1561451DA66165e828F ./contracts/CrashGame.sol:CrashGame --constructor-args $(cast abi-encode "constructor(address)" 0x6F6623B00B0b2eAEFA47A4fDE06d6931F7121722)--verifier blockscout --verifier-url https://sepolia-blockscout.lisk.com/api --chain 4202 --watch 
 
 contract LotteryDeployer is Script {
